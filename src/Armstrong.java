595caaa48c0108792fea44bc7fd3799339aff36e
Saleh <sparsa@atlassian.com>
2016-09-01 00:18:31 +0800

    changed factorial data type to long

---
+++ src/Armstrong.java
@@ -0,0 +1,9 @@
+/**
+ * Created by saleh on 9/1/16.
+ */
+public class Armstrong {
+    /**
+     * An Armstrong number of 3 digit is a number for which sum of cube of its digits are equal to number
+     * e.g. 371 is an Armstrong number because 3*3*3 + 7*7*7 + 1*1*1 = 371).
+     */
+}
